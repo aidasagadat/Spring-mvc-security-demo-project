8341de616c02789289d9f6fff8a4df4ccf50b509
Aida Sagadat <aidasagadat355@gmail.com>
2024-05-10 22:48:57 +0500

    modify spring configuration to reference custom login form

--- src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,12 +34,29 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "CEO")
                 .build();
 
-
         return new InMemoryUserDetailsManager(adiya, dilnaz, aida);
 
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+
+                        form
+                                .loginPage("/ahowMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+
+                        );
+
+        return http.build();
+
+    }
 }
 
 
